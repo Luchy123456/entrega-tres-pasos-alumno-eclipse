32300142770d9161a6896cac659c97b565faca71
Lucia <Lucia@LAPTOP-I8ER62NL>
2022-04-25 19:51:47 +0200

    'añado constructor con parametros rama Lucia '

--- Entornos_ejemplo/src/Alumno.java
+++ Entornos_ejemplo/src/Alumno.java
@@ -4,4 +4,11 @@ public class Alumno {
 	private String nombre;
 	private String apellido;
 	private int DNI;
+	
+// contructor con parametros 
+	public Alumno(String n, String a, int dni) {
+		this.nombre=n;
+		this.apellido=a;
+		this.DNI=dni;
+	}
 }
