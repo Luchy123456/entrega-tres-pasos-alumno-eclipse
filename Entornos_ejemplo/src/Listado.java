474688080ba77464707ca8b80aa59453ed463b5b
Lucia <Lucia@LAPTOP-I8ER62NL>
2022-04-25 19:46:08 +0200

    'Inicio de proyecto'

---
+++ Entornos_ejemplo/src/Listado.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Lucia
+ *
+ */
+public class Listado {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Listado de Alumnos 1ºDAW: ");
+		
+		System.out.println("Alumno 1 :");
+
+	}
+
+}
